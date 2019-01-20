ef0fd95db4eac62b111fa1d66e2bc82f875596d5
Jamaal Francis <jamaal@gmail.com>
2019-01-20 18:06:10 +0000

    Added JSON object

--- js/scripts.js
+++ js/scripts.js
@@ -39,3 +39,91 @@ for (a = 0; a < acc.length; a++) {
     }
   });
 }
+
+const formModal = document.getElementById('myModal');
+const emailInput = document.getElementById('email-input');
+const submitBtnForm = document.getElementById("submitBtnForm");
+const closeBtnForm = document.getElementsByClassName("close")[0];
+
+if (submitBtnForm != null){
+  submitBtnForm.onclick = function() {
+    if (emailInput.value.length > 0) {
+      formModal.style.display = "block";
+    }
+  }
+}
+
+closeBtnForm.onclick = function() {
+  formModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == formModal) {
+    formModal.style.display = "none";
+  }
+}
+
+const viewDetailsButton = document.getElementsByTagName('button');
+if (viewDetailsButton != null){
+  for (let q = 0; q < viewDetailsButton.length; q++) {
+    viewDetailsButton[q].onclick = function() {
+        if (viewDetailsButton[q] == viewDetailsButton[0]){  
+          formModal.style.display = "block";
+        } else if (viewDetailsButton[q] == viewDetailsButton[1]){  
+          formModal.style.display = "block";
+        } else if (viewDetailsButton[q] == viewDetailsButton[2]){  
+          formModal.style.display = "block";
+          loadJSON("Lamborghini");
+        }
+    }
+  }
+}
+
+
+ let user_profile = { 
+  "cars" : [
+    {
+        "model_make_id":"Bugatti",
+        "model_name":"Veyron",
+        "model_trim":"16.4 Grand Sport",
+        "model_year":"2010",
+        "model_engine_fuel":"Gasoline",
+        "model_top_speed_kph":"408",
+        "model_transmission_type":"Automatic",
+        "model_seats":"2",
+        "model_doors":"2",
+        "make_country":"Italy"
+    },
+    {
+      "model_make_id":"McLaren",
+         "model_name":"P1",
+         "model_trim":"2dr Coupe (3.8L 8cyl gas\/electric hybrid 7A)",
+         "model_year":"2016",
+         "model_engine_cc":"3800",
+         "model_engine_fuel":"Premium Unleaded (Required)",
+         "model_transmission_type":"Automated Manual",
+         "make_country":"UK"
+    },
+    {
+      "model_make_id":"Lamborghini",
+      "model_name":"Aventador",
+      "model_trim":"LP 700-4 2dr Coupe AWD (6.5L 12cyl 7AM)",
+      "model_year":"2016",
+      "model_engine_cc":"6500",
+      "model_engine_fuel":"Premium Unleaded (Required)",
+      "model_engine_type":"V",
+      "model_engine_valves_per_cyl":"4",
+      "model_engine_power_ps":"700",
+      "make_country":"Italy"
+    }
+  ]
+}
+
+function loadJSON(carModel) {   
+  document.getElementById("car-heading").innerHTML = user_profile.cars[0].model_make_id + " " +user_profile.cars[0].model_name;
+ }
+  
+ loadJSON();
+
+
+
